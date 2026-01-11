cad148f472f88f5b9b5a85362bf880ecfde455eb
nduggahafizu <nduggahafizu67@gmail.com>
2026-01-11 04:18:54 +0400

    Add PesaPal token test endpoint for debugging

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -38,4 +38,42 @@ router.get('/test/config', (req, res) => {
     });
 });
 
+// Test PesaPal token endpoint
+router.get('/test/pesapal-token', async (req, res) => {
+    const axios = require('axios');
+    const baseUrl = process.env.PESAPAL_ENVIRONMENT === 'live' 
+        ? 'https://pay.pesapal.com/v3'
+        : 'https://cybqa.pesapal.com/pesapalv3';
+    
+    try {
+        const response = await axios.post(
+            `${baseUrl}/api/Auth/RequestToken`,
+            {
+                consumer_key: process.env.PESAPAL_CONSUMER_KEY,
+                consumer_secret: process.env.PESAPAL_CONSUMER_SECRET
+            },
+            {
+                headers: { 'Content-Type': 'application/json' }
+            }
+        );
+        
+        res.json({
+            success: true,
+            message: 'PesaPal token retrieved',
+            tokenReceived: !!response.data?.token,
+            tokenLength: response.data?.token?.length || 0,
+            expiryDate: response.data?.expiryDate,
+            baseUrl: baseUrl
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'PesaPal token error',
+            error: error.message,
+            response: error.response?.data,
+            baseUrl: baseUrl
+        });
+    }
+});
+
 module.exports = router;
